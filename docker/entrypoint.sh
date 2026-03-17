da0146d2cb1052e8ef4c06fe20fa602902193751
Rafah <irafah.alves@gmail.com>
2026-03-17 13:14:01 -0300

    Fix deploy - port issues

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -25,5 +25,8 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+echo "[entrypoint] Configuring nginx port ${PORT:-80}..."
+sed -i "s/\${PORT}/${PORT:-80}/" /etc/nginx/conf.d/default.conf
+
 echo "[entrypoint] Starting server..."
 exec "$@"
